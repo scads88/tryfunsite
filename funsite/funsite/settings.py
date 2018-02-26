e9f240609b97ce9b8c46f3c431e30326459af385
John McCarthy <john3mccarthy@gmail.com>
2018-02-26 17:37:39 +0000

    here we have new django with debug=false for pdxn

--- funsite/funsite/settings.py
+++ funsite/funsite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'x-d$!4&o_=s9azrwh%+#8w4ov=gz)&2mgl9-@d03hbd%+mhu+*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
